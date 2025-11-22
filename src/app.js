0398eddbd56bae53010bd93173b5285e15cddb35
Ananda salwa pratama <nandev1915@gmail.com>
2025-11-22 20:10:07 +0700

    nothing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import app from "./server.js";
 
 const PORT = 8080;
-const IPADDRESS = "10.159.38.155";
+const IPADDRESS = "192.168.1.5";
 
 app.listen(PORT, IPADDRESS, () => {
   console.log(`Server Running on http://${IPADDRESS}:${PORT}`);
